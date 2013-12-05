c5f3425309ee1358760690a6c02dd1fc947d3aa4
Katie McNabb <katiemcnabb@gmail.com>
2013-12-05 12:19:38 -0500

    working on remote integration

--- OnCar/Challenge8-OnCar/src/org/sunspotworld/SunSpotApplication.java
+++ OnCar/Challenge8-OnCar/src/org/sunspotworld/SunSpotApplication.java
@@ -1,7 +1,9 @@
 package org.sunspotworld;
 
 import com.sun.spot.io.j2me.radiogram.*;
-import com.sun.spot.peripheral.radio.RadioFactory;
+import com.sun.spot.peripheral.Spot;
+import com.sun.spot.peripheral.TimeoutException;
+import com.sun.spot.peripheral.radio.IRadioPolicyManager;
 import com.sun.spot.resources.Resources;
 import com.sun.spot.resources.transducers.IAnalogInput;
 import com.sun.spot.resources.transducers.ISwitchListener;
@@ -13,9 +15,8 @@ import com.sun.spot.sensorboard.EDemoBoard;
 import com.sun.spot.sensorboard.peripheral.IServo;
 import com.sun.spot.sensorboard.peripheral.ISwitch;
 import com.sun.spot.sensorboard.peripheral.Servo;
-import com.sun.spot.service.BootloaderListenerService;
-import com.sun.spot.util.IEEEAddress;
 import com.sun.spot.util.Utils;
+import java.io.IOException;
 
 import javax.microedition.io.Connector;
 import javax.microedition.io.Datagram;
@@ -28,19 +29,24 @@ import javax.microedition.midlet.MIDletStateChangeException;
  */
 public class SunSpotApplication extends MIDlet implements ISwitchListener {
     
+    private static final short PAN_ID               = IRadioPolicyManager.DEFAULT_PAN_ID; 
+    private static final String BROADCAST_PORT      = "161"; 
+    private static final int PACKETS_PER_SECOND     = 1; 
+    private static final int PACKET_INTERVAL        = 3000 / PACKETS_PER_SECOND; 
+    private int channel = 21; 
+    private int power = 32;  
     // devices
     private EDemoBoard eDemo = EDemoBoard.getInstance();
     private ISwitch sw2 = (ISwitch)Resources.lookup(ISwitch.class, "SW2"); 
-    private ISwitch sw1 = (ISwitch)Resources.lookup(ISwitch.class, "SW1"); 
-    
-    // left servo from driver's view
-    private IServo servo1 = new Servo(eDemo.getOutputPins()[EDemoBoard.H1]);
-    
-    // right servo from driver's view
-    private IServo servo2 = new Servo(eDemo.getOutputPins()[EDemoBoard.H0]);
+    private IServo servo1 = new Servo(eDemo.getOutputPins()[EDemoBoard.H1]); //moves car right/left
+    private IServo servo2 = new Servo(eDemo.getOutputPins()[EDemoBoard.H0]); //moves car forward/backward
     
     public static final double voltage = 5.0; 
     public static final double scaleFactor = voltage/512;
+    
+    private LEDColor red   = new LEDColor(50,0,0); 
+    private LEDColor green = new LEDColor(0,50,0); 
+    private LEDColor blue  = new LEDColor(0,0,50);
     
     private static final int HOST_PORT = 65;
     private ITriColorLEDArray leds = (ITriColorLEDArray) Resources.lookup(ITriColorLEDArray.class);
@@ -49,70 +55,190 @@ public class SunSpotApplication extends MIDlet implements ISwitchListener {
     private IAnalogInput rightCarSensor = EDemoBoard.getInstance().getAnalogInputs()[EDemoBoard.A0];
     private IAnalogInput leftCarSensor = EDemoBoard.getInstance().getAnalogInputs()[EDemoBoard.A1];
     
+    private boolean driveSelf = false;
+    private boolean override = false;
+    private int caseNum;
+    
+    private double srcXtilt;
+    private double srcYtilt;
+
     protected void startApp() throws MIDletStateChangeException {
-        
-
-        BootloaderListenerService.getInstance().start();   // monitor the USB (if connected) and recognize commands from host
-
-        long ourAddr = RadioFactory.getRadioPolicyManager().getIEEEAddress();
-        System.out.println("Our radio address = " + IEEEAddress.toDottedHex(ourAddr));
-        
-        try {
-            
-            // Open up a broadcast connection to the host port
-            // where the 'on Desktop' portion of this demo is listening
-            rCon = (RadiogramConnection) Connector.open("radiogram://broadcast:" + HOST_PORT);
-            dg = rCon.newDatagram(50);  // only sending 12 bytes of data
-        } catch (Exception e) {
-            System.err.println("Caught " + e + " in connection initialization.");
-            notifyDestroyed();
-        }
-        
-        
-        //calibrate
-        while (sw2.isOpen())
-        {
-
-            leds.getLED(2).setColor(new LEDColor(255,0,0));    
-               leds.getLED(2).setOn();
-               servo1.setValue(1540);
-               servo2.setValue(1600);
-        }
-        
-        leds.getLED(2).setOff();
-        
-        //drive forward
-        servo2.setValue(1530);
-         while(true){
-       try {
-            
-           dg.reset();
-                double inchesCarRight = (rightCarSensor.getVoltage());
-                double inchesCarLeft = (leftCarSensor.getVoltage());
-                dg.writeDouble(inchesCarLeft);
-                dg.writeDouble(inchesCarRight);
-
-                rCon.send(dg);
-                if (inchesCarRight > .9) {
-                    slideRight();
-                    Utils.sleep(1200);
-                }
-                else if (inchesCarLeft > 1.1) {
-                    slideLeft();
-                    Utils.sleep(1200);
-                }
-                else {
-         
-                }
-            } catch (Exception ex){
-                ex.printStackTrace();
-            }
-       
-        }
+     
+        initialize();
+        run();
      }
+    
+    
+    /*
+     * loop to receive data from the 
+     */
+    private void recvLoop () {
+        RadiogramConnection rcvConn = null; 
+        boolean recvDo = true; 
+        int nothing = 0; 
+        while (recvDo) { 
+            try { 
+                rcvConn = (RadiogramConnection)Connector.open("radiogram://:" + BROADCAST_PORT); 
+                rcvConn.setTimeout(PACKET_INTERVAL - 5); 
+                Radiogram rdg = (Radiogram)rcvConn.newDatagram(rcvConn.getMaximumLength()); 
+                  
+                while (recvDo) {
+                    try {   
+                            rdg.reset(); 
+                            rcvConn.receive(rdg);
+                            long srcAddr = rdg.readLong(); // src MAC address 
+                            srcXtilt = rdg.readDouble(); // src's STEER 
+                            srcYtilt = rdg.readDouble();
+                            override = rdg.readBoolean(); // are we getting an override message from remote
+                            driveSelf = rdg.readBoolean(); // drive self command from remote
+                            System.out.println("xtilt is" +srcXtilt);
+                            
+                            System.out.println("drive self is " +driveSelf);
+                            System.out.println("override is " +override);
+                    } 
+                    catch (TimeoutException tex) {        // timeout - display no packet received 
+                        leds.getLED(0).setColor(red); 
+                        leds.getLED(0).setOn(); 
+                        nothing++; 
+                        if (nothing > 2 * PACKETS_PER_SECOND) { 
+                            for (int ledint = 0; ledint<=7; ledint++){ // if nothing received eventually turn off LEDs 
+                                leds.getLED(ledint).setOff(); 
+                            } 
+                        } 
+                    } 
+                } 
+            } catch (IOException ex) { 
+                // ignore 
+            } finally { 
+                if (rcvConn != null) { 
+                    try { 
+                        rcvConn.close(); 
+                    } catch (IOException ex) { } 
+                } 
+            } 
+        } 
+    } 
 
+    private void stateMachineLoop()
+    {
+        while (true)
+        {
+            if (override == false && driveSelf == false)
+            {
+                caseNum = 1;
+            }
+            else if (driveSelf == true && override == false)
+            {
+                caseNum = 2;
+            }
+            
+            /*an override command always take precendence*/
+            else if (override == true)
+            {
+                caseNum = 3;
+            }
+            
+            switch(caseNum)
+            {
+                //calibrates
+                case 1:
+                {
+                   //middle light green indicates calibration
+                   leds.getLED(2).setColor(green);    
+                   leds.getLED(2).setOn();
+                                       
+                   servo1.setValue(1540);
+                   servo2.setValue(1600);
+                   System.out.println("case 1");
+                   break;
+                }//end case 1 code
+
+                    
+                //case 2 car drives itself
+                case 2:
+                {
+                    System.out.println("case 2");
+                    leds.getLED(2).setOff();
+                    leds.getLED(7).setOff();
+                    leds.getLED(0).setColor(green);
+                    leds.getLED(0).setOn();
+                    
+                    //drive forward
+                    servo2.setValue(1530);
+
+                    try {
+                    
+                    double inchesCarRight = (rightCarSensor.getVoltage());
+                    double inchesCarLeft = (leftCarSensor.getVoltage());
+                    
+                    if (inchesCarRight > .9) {
+                        slideRight();
+                        Utils.sleep(1200);
+                    }
+                    else if (inchesCarLeft > 1.1) {
+                        slideLeft();
+                        Utils.sleep(1200);
+                    }
+                } catch (Exception ex){
+                    ex.printStackTrace();
+                }
+
+
+                break;
+              }//end case 2 code
+                    
+                    
+                //case three is drive based on remote command     
+                case 3:
+                {
+                    System.out.println("Case 3");
+                    leds.getLED(2).setOff();
+                    leds.getLED(0).setOff();
+                    leds.getLED(7).setColor(green);
+                    leds.getLED(7).setOn();
+                    
+                    int servo1Remote = (int)(1540 - 180*srcXtilt);
+                    int servo2Remote = (int) (1540 - 180*srcYtilt);
+                    servo1.setValue(servo1Remote);
+                    servo2.setValue(servo2Remote);
+                    
+                    break;
+                }//end case 3 code
+            }
+        }
+    }
     protected void pauseApp() {
         // This is not currently called by the Squawk VM
+    }
+     
+    /** 
+     * Initialize any needed variables. 
+     */
+    private void initialize() {  
+        
+        IRadioPolicyManager rpm = Spot.getInstance().getRadioPolicyManager(); 
+        rpm.setChannelNumber(channel); 
+        rpm.setPanId(PAN_ID); 
+        rpm.setOutputPower(power - 32); 
+    } 
+    
+    /*
+     * run various threads
+     */
+    private void run() { 
+  
+        new Thread() { 
+            public void run () { 
+                recvLoop(); 
+            } 
+        }.start();                      // spawn a thread to receive packets 
+
+        new Thread() { 
+            public void run () { 
+                stateMachineLoop(); 
+            } 
+        }.start();                      // spawn a thread to operate car control state machine
+ 
     }
 
     protected void destroyApp(boolean unconditional) throws MIDletStateChangeException {
---
+++ RemoteControlFinalChallenge/src/org/sunspotworld/RemoteSunSpotApplication.java
@@ -0,0 +1,231 @@
+package org.sunspotworld; 
+  
+import com.sun.spot.peripheral.Spot; 
+import com.sun.spot.peripheral.TimeoutException; 
+import com.sun.spot.peripheral.radio.IRadioPolicyManager; 
+import com.sun.spot.peripheral.radio.RadioFactory; 
+import com.sun.spot.resources.Resources; 
+import com.sun.spot.resources.transducers.ILed; 
+import com.sun.spot.resources.transducers.ISwitch; 
+import com.sun.spot.resources.transducers.ITriColorLED; 
+import com.sun.spot.resources.transducers.LEDColor; 
+import com.sun.spot.resources.transducers.ITriColorLEDArray; 
+import com.sun.spot.resources.transducers.IAccelerometer3D; 
+import com.sun.spot.io.j2me.radiogram.Radiogram; 
+import com.sun.spot.io.j2me.radiogram.RadiogramConnection; 
+import com.sun.spot.resources.transducers.ISwitchListener;
+import com.sun.spot.resources.transducers.SwitchEvent;
+  
+import java.io.IOException; 
+import javax.microedition.io.Connector; 
+import javax.microedition.io.Datagram; 
+import javax.microedition.midlet.MIDlet; 
+import javax.microedition.midlet.MIDletStateChangeException; 
+  
+
+/*Code for on spot that is the remote*/
+public class RemoteSunSpotApplication extends MIDlet implements ISwitchListener{ 
+  
+    private static final String VERSION = "1.0"; 
+    // CHANNEL_NUMBER  default as 26, each group set their own correspondingly 
+    //private static final int CHANNEL_NUMBER = IProprietaryRadio.DEFAULT_CHANNEL;  
+    private static final short PAN_ID               = IRadioPolicyManager.DEFAULT_PAN_ID; 
+    private static final String BROADCAST_PORT      = "161"; 
+    private static final int PACKETS_PER_SECOND     = 1; 
+    private static final int PACKET_INTERVAL        = 3000 / PACKETS_PER_SECOND; 
+      
+    private int channel = 21; 
+    private int power = 32;                             // Start with max transmit power 
+      
+    private ISwitch sw1 = (ISwitch)Resources.lookup(ISwitch.class, "SW1"); 
+    private ISwitch sw2 = (ISwitch)Resources.lookup(ISwitch.class, "SW2"); 
+    private ITriColorLEDArray leds = (ITriColorLEDArray)Resources.lookup(ITriColorLEDArray.class); 
+    private ITriColorLED statusLED = leds.getLED(0); 
+    
+    private IAccelerometer3D accel = (IAccelerometer3D)Resources.lookup(IAccelerometer3D.class); 
+  
+    private LEDColor red   = new LEDColor(50,0,0); 
+    private LEDColor green = new LEDColor(0,50,0); 
+    private LEDColor blue  = new LEDColor(0,0,50); 
+    private double xTilt;
+    private double yTilt;
+    private boolean override = false;
+    private boolean driveSelf = false;
+    
+    private long myAddr = 0; // own MAC addr (ID) 
+    private long save_addr[] = {0,0,0,0,0,0};// save all the MAC linked to spot 
+    
+      
+    private boolean xmitDo = true; 
+      
+        /*transmit driving instructions to car*/
+        private void xmitLoop () { 
+        RadiogramConnection txConn = null; 
+        xmitDo = true; 
+        
+        while (xmitDo) { 
+            try { 
+                txConn = (RadiogramConnection)Connector.open("radiogram://broadcast:" + BROADCAST_PORT); 
+                txConn.setMaxBroadcastHops(1);      // don't want packets being rebroadcasted 
+                Datagram xdg = txConn.newDatagram(txConn.getMaximumLength()); 
+                while (xmitDo) { 
+  
+                    xdg.reset(); 
+                    xdg.writeLong(myAddr); // own MAC address 
+                    xdg.writeDouble(xTilt); // xtilt
+                    xdg.writeDouble(yTilt); // ytilt 
+                    xdg.writeBoolean(override);
+                    xdg.writeBoolean(driveSelf);
+                    txConn.send(xdg); 
+                          
+                    
+                System.out.println("xtilt is" + xTilt);
+                }
+            }
+                catch (IOException ex) { 
+                // ignore 
+            } finally { 
+                if (txConn != null) { 
+                    try { 
+                        txConn.close(); 
+                    } catch (IOException ex) { } 
+                } 
+            } 
+        } 
+    } 
+      
+      
+    private void pause (long time) { 
+        try { 
+            Thread.currentThread().sleep(time); 
+        } catch (InterruptedException ex) { /* ignore */ } 
+    } 
+      
+  
+    /** 
+     * Initialize any needed variables. 
+     */
+    private void initialize() {  
+        myAddr = RadioFactory.getRadioPolicyManager().getIEEEAddress();  
+        statusLED.setColor(red);     // Red = not active 
+        statusLED.setOn(); 
+        IRadioPolicyManager rpm = Spot.getInstance().getRadioPolicyManager(); 
+        rpm.setChannelNumber(channel); 
+        rpm.setPanId(PAN_ID); 
+        rpm.setOutputPower(power - 32); 
+        sw2.addISwitchListener(this);
+        sw1.addISwitchListener(this);
+    } 
+        
+    /** 
+     * Main application run loop. 
+     */
+    private void run() { 
+  
+        
+        new Thread() { 
+            public void run () { 
+                xmitLoop(); 
+            } 
+        }.start();                      // spawn a thread to transmit packets 
+ 
+      new Thread() { 
+            public void run () { 
+                LEDLoop(); 
+            } 
+        }.start();
+      
+    } 
+   
+  private void LEDLoop() 
+  {
+      while (true)
+      {
+        try {
+            xTilt = accel.getTiltX();
+            yTilt = accel.getTiltY();
+            
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+                
+                if (xTilt > 0)
+                    {
+                        leds.getLED(0).setColor(blue);
+                        leds.getLED(0).setOn();
+                        leds.getLED(7).setOff();
+                    }
+                    else
+                    {
+                        leds.getLED(7).setColor(red);
+                        leds.getLED(7).setOn();
+                        leds.getLED(0).setOff();
+                    }
+      }
+  }
+  
+  
+   /*handle switch 2 pressed event*/
+    public void switchPressed(SwitchEvent Sw) {
+    
+      if (Sw.getSwitch().equals(sw2) && override == false)
+      {
+          /*overriding autonomous control*/
+          leds.getLED(6).setColor(green);
+          leds.getLED(6).setOn();
+          override = true;
+      }
+      else if (Sw.getSwitch().equals(sw2))
+      {
+          leds.getLED(6).setOff();
+          override = false;
+          
+      }
+      else if (Sw.getSwitch().equals(sw1) && driveSelf == false)
+      {
+          /*overriding autonomous control*/
+          leds.getLED(1).setColor(green);
+          leds.getLED(1).setOn();
+          driveSelf = true;
+      }
+      else if (Sw.getSwitch().equals(sw1))
+      {
+          leds.getLED(1).setOff();
+          driveSelf = false;
+          
+      }
+      
+  
+    }
+      
+    /** 
+     * MIDlet call to start our application. 
+     */
+    protected void startApp() throws MIDletStateChangeException { 
+    // Listen for downloads/commands over USB connection 
+    new com.sun.spot.service.BootloaderListenerService().getInstance().start(); 
+        initialize(); 
+        run(); 
+    } 
+  
+    /** 
+     * This will never be called by the Squawk VM. 
+     */
+    protected void pauseApp() { 
+        // This will never be called by the Squawk VM 
+    } 
+  
+    /** 
+     * Called if the MIDlet is terminated by the system. 
+     * @param unconditional If true the MIDlet must cleanup and release all resources. 
+     */
+    protected void destroyApp(boolean unconditional) throws MIDletStateChangeException { 
+    } 
+
+
+    /*remain not implemented*/
+    public void switchReleased(SwitchEvent se) {
+     
+    }
+  
+}
