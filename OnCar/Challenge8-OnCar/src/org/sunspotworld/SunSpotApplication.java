9b88fee1f4ebc6e0b5168a12e86e23e0df839d4c
Calvin Flegal <calvin.flegal@gmail.com>
2013-12-04 21:18:29 -0500

    increased turning and center to right slighly (10 more)

--- OnCar/Challenge8-OnCar/src/org/sunspotworld/SunSpotApplication.java
+++ OnCar/Challenge8-OnCar/src/org/sunspotworld/SunSpotApplication.java
@@ -41,6 +41,12 @@ public class SunSpotApplication extends MIDlet implements ISwitchListener {
     
     public static final double voltage = 5.0; 
     public static final double scaleFactor = voltage/512;
+    private double leftDistance;
+    private double rightDistance;
+    private double p1 = -10.76;
+    private double p2 = 67.71;
+    private double p3 = -163.9;
+    private double p4 = 169.3;
     
     private static final int HOST_PORT = 65;
     private ITriColorLEDArray leds = (ITriColorLEDArray) Resources.lookup(ITriColorLEDArray.class);
@@ -75,7 +81,7 @@ public class SunSpotApplication extends MIDlet implements ISwitchListener {
 
             leds.getLED(2).setColor(new LEDColor(255,0,0));    
                leds.getLED(2).setOn();
-               servo1.setValue(1540);
+               servo1.setValue(1550);
                servo2.setValue(1600);
         }
         
@@ -88,20 +94,22 @@ public class SunSpotApplication extends MIDlet implements ISwitchListener {
             double leftAvg = 0;
             double rightAvg = 0;
             for (int i=0; i<3; i++) {
-                leftAvg += (leftCarSensor.getVoltage())*(.3333);
-                rightAvg += (rightCarSensor.getVoltage())*(.3333);
-                Utils.sleep(100);
+                double leftVoltage = leftCarSensor.getVoltage();   
+                leftDistance = p1 * (pow(leftVoltage, 3)) + p2 * (pow(leftVoltage, 2)) + p3 * leftVoltage + p4;
+                double rightVoltage = rightCarSensor.getVoltage();   
+                rightDistance = p1 * (pow(rightVoltage, 3)) + p2 * (pow(rightVoltage, 2)) + p3 * rightVoltage + p4;
+                leftAvg += leftDistance*.3333;
+                rightAvg += rightDistance*(.3333);
+                Utils.sleep(50);
             }
-            double inchesCarRight = leftAvg;
-            double inchesCarLeft = rightAvg;
-            System.out.println("left avg is: " + leftAvg);
-             System.out.println("right avg is: " + rightAvg);
+            System.out.println("left avg distance: " + leftAvg + " cm");
+             System.out.println("right avg is: " + rightAvg + " cm");
  
-                if (inchesCarRight > 1.3) {
+                if (leftAvg < 56) {
                     slideRight();
                     Utils.sleep(1200);
                 }
-                else if (inchesCarLeft > 1.3) {
+                else if (rightAvg < 60) {
                     slideLeft();
                     Utils.sleep(1200);
                 }
@@ -133,7 +141,7 @@ public class SunSpotApplication extends MIDlet implements ISwitchListener {
        Thread.sleep(900);
        
        //straighten
-       servo1.setValue(1540);
+       servo1.setValue(1550);
     }
     
     public void slideRight() throws InterruptedException
@@ -147,7 +155,7 @@ public class SunSpotApplication extends MIDlet implements ISwitchListener {
        Thread.sleep(1000);
        
        //straighten
-       servo1.setValue(1540);
+       servo1.setValue(1550);
 
     }
     public void switchReleased(SwitchEvent se) {
@@ -156,4 +164,40 @@ public class SunSpotApplication extends MIDlet implements ISwitchListener {
 
     public void switchPressed(SwitchEvent se) {
 }
+    public double pow(double x, double y) {
+        int den = 1024; //declare the denominator to be 1024  
+        /*Conveniently 2^10=1024, so taking the square root 10  
+        times will yield our estimate for n.??In our example  
+        n^3=8^2n^1024 = 8^683.*/
+        int num = (int) (y * den); // declare numerator
+        int iterations;
+        iterations = 10;
+        double n = Double.MAX_VALUE; /* we initialize our
+         * estimate, setting it to max*/
+        while (n >= Double.MAX_VALUE && iterations > 1) {
+            /*??We try to set our estimate equal to the right
+             * hand side of the equation (e.g., 8^2048).??If this
+             * number is too large, we will have to rescale. */
+            n = x;
+            for (int i = 1; i < num; i++) {
+                n *= x;
+            }
+            /*here, we handle the condition where our starting
+             * point is too large*/
+            if (n >= Double.MAX_VALUE) {
+                iterations--;
+                den = (int) (den / 2);
+                num = (int) (y * den); //redefine the numerator
+            }
+        }
+        /*************************************************  
+         ** We now have an appropriately sized right-hand-side.  
+         ** Starting with this estimate for n, we proceed.  
+         **************************************************/
+        for (int i = 0; i < iterations; i++) {
+            n = Math.sqrt(n);
+        }
+        // Return our estimate
+        return n;
+    }
 }
